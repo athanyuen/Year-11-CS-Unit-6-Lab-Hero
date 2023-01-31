a1d1697e22ccce321ea4e5a16902516142be8baf
techarenz <57818506+techarenz@users.noreply.github.com>
2023-01-31 19:17:40 +0800

    Update README.md

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Hero opponent = new Hero("yes");
+        Hero name = new Hero("no");
+        name.dramaticFightToTheDeath(opponent);
+    }
+}
